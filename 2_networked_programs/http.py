7115f2789a3af41e4f1236cbf733470f430fb8a6
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-12 12:03:22 +0300

    using urllib

--- 2_networked_programs/http.py
+++ 2_networked_programs/http.py
@@ -2,6 +2,9 @@ import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('www.py4inf.com', 80))
 mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+# mysock.connect(('www.amoiteumot.wordpress.com', 80))
+# mysock.send('GET https://www.amoiteumot.wordpress.com/2016/05/11/what-potential/ HTTP/1.0\n\n')
+'''prints html'''
 
 while True:
 	data = mysock.recv(512)
---
+++ 2_networked_programs/using_urllib.py
@@ -0,0 +1,13 @@
+import urllib
+import re
+
+fhand = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
+for line in fhand:
+	print line.rstrip()
+
+# url = raw_input('Enter - ')
+# html = urllib.urlopen(url).read()
+# links = re.findall('href="(http://.*?)"', html)
+# for link in links:
+# 	print link
+
