d80083c632e3e022aeac10c7f4aee23713301915
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-17 14:20:18 +0300

    Regular expressions with inline comments (VERBOSE)

--- 1_regex_exercises/roman_to_int.py
+++ 1_regex_exercises/roman_to_int.py
@@ -10,13 +10,20 @@ def roman_to_int(s):
 			"IX": 9,   "V" : 5,   "IV": 4,   "I" : 1 
 			}
 
-	pattern = ('^(M{0,4})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$')
+	pattern = '''
+		^					# Beginning of string
+		(M{0,4})			# Thousands - 0-4 M's
+		(CM|CD|D?C{0,3})	#Hundreds: CM or CD or optional D followed by 3 optional C's 
+		(XC|XL|L?X{0,3})	#Tens: XC or XL or optional L and 3 optional X's
+		(IX|IV|V?I{0,3})	#Ones: IX or IV or optional V and 3 optional I's
+		$					# End of string
+		'''
 	
-	if not re.search (pattern, s):
+	if not re.search (pattern, s, re.VERBOSE):
 		return 'Invalid Roman Numeral'
 	
 	result = 0
-	for item in re.findall(pattern, s):
+	for item in re.findall(pattern, s, re.VERBOSE):
 		for i in item:
 			if i == '':
 				continue
