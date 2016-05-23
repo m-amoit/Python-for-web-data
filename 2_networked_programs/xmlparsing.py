183783dadcc5b5194ea94c64cb1997353bbb6e18
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-23 13:49:29 +0300

    Looping through nodes

--- 2_networked_programs/xmlparsing.py
+++ 2_networked_programs/xmlparsing.py
@@ -2,15 +2,25 @@ import xml.etree.ElementTree as ET
 #allows extraction of data from XML without worrying about 
 #the rules of XML
 
-data = '''
-	<person>
-		<name>Chuck</name>
-		<phone type = "intl">+254 729 240 618</phone>
-		<email hide = "yes"/>
-	</person>'''
+input = '''
+	<stuff>
+		<users>
+			<user x="2">
+				<id>001</id>
+				<name>Chuck</name>
+			</user>
+			<user x="7">
+				<id>009</id>
+				<name>Brent</name>
+			</user>
+		</users>
+	</stuff>
+	'''
 
-tree = ET.fromstring(data)
-#Converts string representation of XML into a tree of XML nodes
-print 'Name:',tree.find('name').text
-#retrieve a node that matches the specified tag
-print 'Attr:',tree.find('email').get('hide')
+stuff = ET.fromstring(input)
+l = stuff.findall('users/user')
+print 'User count:', len(l)
+
+for item in l:
+	print ('Name: %s Id: %s Attr: %s') % \
+	(item.find('name').text, item.find('id').text, item.get('x'))
