1029d934b53a7a5f3c4d958064b1a4d0fd8612ad
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-25 23:01:27 +0300

    Basic SQL commands

--- 3_databases_and_SQL/create_table.py
+++ 3_databases_and_SQL/create_table.py
@@ -13,14 +13,26 @@ conn.commit() #commit() writes data to database file
 print 'Tracks:'
 
 '''Retrieve rows from table'''
-cur.execute('SELECT title, plays FROM Tracks')
+# cur.execute('SELECT title, plays FROM Tracks')
+''' * returns all columns for each row that matches the WHERE clause 
+logical operators: =, <, >, <=, >=, !=, AND, OR'''
+# cur.execute('SELECT * FROM Tracks WHERE title = "My Way"')
+
+'''optional ORDER BY to control sorting of the returned rows'''
+cur.execute('SELECT title, plays FROM Tracks ORDER BY plays')
+
 for row in cur: #cursor() is iterable. Data read on demand as loop executes
 	print row
-
 #output: u' - unicode strings capable of storing non-Latin character sets
 
-# cur.execute('DELETE FROM Tracks WHERE plays < 100')
-# conn.commit()
+'''Update a column within 1+ rows in a table. Where specifies rows to be
+updated. Else all rows will be updated.'''
+cur.execute('UPDATE Tracks SET plays = 16 WHERE title = "My Way"')
+conn.commit()
+
+'''Remove rows, WHERE specifies which rows to delete'''
+cur.execute('DELETE FROM Tracks WHERE plays < 100')
+conn.commit()
 
 cur.close()
 
