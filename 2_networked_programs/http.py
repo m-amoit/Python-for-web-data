c82a2de7e8804ae504b91f75831fed33f5638eb1
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-25 18:30:19 +0300

    making a low level network connection with sockets

--- 2_networked_programs/http.py
+++ 2_networked_programs/http.py
@@ -1,12 +1,13 @@
+'''making a low level network connection with sockets'''
 import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.py4inf.com', 80))
+mysock.connect(('www.py4inf.com', 80)) #makes a connection to port 80 on the server www.py4info.com
 mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
-# mysock.connect(('www.amoiteumot.wordpress.com', 80))
-# mysock.send('GET https://www.amoiteumot.wordpress.com/2016/05/11/what-potential/ HTTP/1.0\n\n')
 '''prints html'''
 
 while True:
+	'''data is received in 512 character chunks until there's 
+	no more data to be read - recv() returns an empty string'''
 	data = mysock.recv(512)
 	if (len(data) < 1):
 		break
