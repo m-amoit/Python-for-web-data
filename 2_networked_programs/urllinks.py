6114266b209b948567975c98b3c5a6e214dc9efb
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-27 00:40:10 +0300

    parsing html using BeautifulSoup

---
+++ 2_networked_programs/urllinks.py
@@ -0,0 +1,15 @@
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter - ')
+
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+# Retrieve a list of anchor tags
+# Each tag is like a dictionary of HTML attributes
+
+tags = soup('a')
+
+for tag in tags:
+	print tag.get('href', None)
