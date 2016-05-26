d9ab49e1ba9ba8ceb35448c081b24b165c8843bd
Miriam Amoit <amoitjmiriam@gmail.com>
2016-05-26 23:39:41 +0300

    Reading web pages

--- 2_networked_programs/using_urllib.py
+++ 2_networked_programs/using_urllib.py
@@ -1,10 +1,19 @@
 import urllib
 import re
 
-fhand = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
-for line in fhand:
-	print line.rstrip()
+# a library that does all the socket work and makes web pages look like files.
+# compare output with http.py
 
+fhand = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
+# for line in fhand:
+# 	print line.rstrip()
+counts = dict()
+for line in fhand:
+	for word in line.split():
+		counts[word] = counts.get(word, 0) + 1
+print counts
+
+'''extracting links'''
 # url = raw_input('Enter - ')
 # html = urllib.urlopen(url).read()
 # links = re.findall('href="(http://.*?)"', html)
